14071d8ca7215949a3ff9195c10eb78e0bbb893d
sultanymbae <ns12251@auca.kg>
2025-01-30 11:56:15 +0600

    lab07

---
+++ lab07/problem01.py
@@ -0,0 +1,21 @@
+#1. Print numbers from 1 to 10 using both for and while loops
+#Using for and range
+
+for i in range(1, 11): #Iterate from 0 to 10
+    print(i)
+
+#Using while
+i = 1
+while i <= 10:
+    print(i)
+    i += 1 #To avoid infinite loop
+
+#2. Use for loop to iterate over the string "Python" and print each character
+for char in "Python":
+    print(char)
+
+#3. Create a while loop that prints even numbers from 2 to 10
+i =2
+while i <= 10:
+    print(i)
+    i += 2
